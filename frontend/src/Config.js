64b1ec781a19b094867a06c670a34b016b1b379a
Shauna Zhang <shengchenzhang1207@gmail.com>
2020-10-07 21:10:50 +0800

    Update Config.js

--- frontend/src/Config.js
+++ frontend/src/Config.js
@@ -7,15 +7,15 @@ function getThemeColor(color, opacity) {
 }
 
 export default {
-    simulateApi: "https://workshop.citysciencelabshanghai.media/api/start",
-    statusApi: "https://workshop.citysciencelabshanghai.media/api/status",
-    resultsApi: "response.json",
-    stopApi: "https://workshop.citysciencelabshanghai.media/api/stop",
+    // simulateApi: "https://workshop.citysciencelabshanghai.media/api/start",
+    // statusApi: "https://workshop.citysciencelabshanghai.media/api/status",
+    // resultsApi: "response.json",
+    // stopApi: "https://workshop.citysciencelabshanghai.media/api/stop",
 
-    // simulateApi: "/api/start",
-    // statusApi: "/api/status",
-    // resultsApi: "/api/result",
-    // stopApi: "/api/stop",
+    simulateApi: "/api/start",
+    statusApi: "/api/status",
+    resultsApi: "/api/result",
+    stopApi: "/api/stop",
     incentiveModes: ['No incentives', 'Static incentives', 'Dynamic incentives'],
     nonePanels: [
         {
